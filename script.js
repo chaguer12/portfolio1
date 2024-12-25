458f33f6426902a6c60d48eac9ed0b61b1d5e24f
zoolotoui <chag.med.amine@gmail.com>
2024-12-25 14:29:16 +0100

    stat: deploying

--- script.js
+++ script.js
@@ -4,3 +4,34 @@ function toggleMenu(){
     menu.classList.toggle('open')
     icon.classList.toggle('open')
 }
+
+
+document.addEventListener("DOMContentLoaded", () => {
+    const text = "Hello, I'm"; // Text to animate
+    const element = document.getElementById("animatedText");
+    let index = 0;
+
+    function typeText() {
+        if (index < text.length) {
+            element.textContent += text.charAt(index);
+            index++;
+            setTimeout(typeText, 100); // Adjust the delay for typing speed
+        } else {
+            setTimeout(() => {
+                clearText();
+            }, 1000); // Wait before clearing the text
+        }
+    }
+
+    function clearText() {
+        if (index > 0) {
+            element.textContent = text.substring(0, index - 1);
+            index--;
+            setTimeout(clearText, 50); // Adjust the delay for clearing speed
+        } else {
+            typeText(); // Restart typing
+        }
+    }
+
+    typeText();
+});
